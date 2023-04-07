7ee989862b123be30dd721e965ff22bc63b814f2
alex-mazov <alexander.mazov@gmail.com>
2023-04-07 16:39:25 +0600

    packages: custom add form

--- packages/forms.py
+++ packages/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from . import models
 class AddPackage(forms.Form):
     trackid = forms.CharField(max_length=50, label='Номер трека')
-    #trackid = forms.RegexField("/^[A-Za-z0-9]+$/",max_length=50,label='Трек-номер')
-    desc = forms.CharField(help_text="Можно добавить описание", widget=forms.Textarea, required=False)
+    desc = forms.CharField(help_text="Можно добавить описание", widget=forms.Textarea, required=False, label='Описание')
 
 

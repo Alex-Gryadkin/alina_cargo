33ea783162b5d0e1320759f4f684a4c03c98282c
Alex <thssfrlrnng@gmail.com>
2023-04-08 19:26:12 +0600

    added auth which leads to authorised zone

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+app_name = 'accounts'
 
 urlpatterns = [
-    path('', views.login, name='login'),
+    path('', views.login_view, name='login'),
 
 
 ]

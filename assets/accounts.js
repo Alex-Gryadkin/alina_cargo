996b72bf26c962c9058e1232d56784630d93167f
alex-mazov <alexander.mazov@gmail.com>
2023-04-12 04:44:54 +0600

    JS: return main.js for common functions, fix for erroralert, theme auto-switch

--- assets/accounts.js
+++ assets/accounts.js
@@ -1,20 +1,6 @@
 var OpenOTPEl = new bootstrap.Modal(document.getElementById('OpenOTPModal'))
 
-function errorAlert(errorMessage,inputId){
-    $('#'+inputId).after('<div id="alertfor' + inputId + '" class="erroralert invalid-feedback">' + errorMessage + '</div')
-    $('#alertfor' + inputId).fadeIn('slow')
-    if (inputId=='') {return}
-    $('#'+inputId).addClass('is-invalid')
-
-}
-function formReset(formId){
-    $('#'+formId).trigger('reset')
-    $(':input','#'+formId).removeClass('is-invalid')
-    $('.erroralert','#'+formId).remove()
-}
-
 $(document).ready(function(){
-
 
     $('#OpenOTPBtn').click(function(){
         OpenOTPEl.show()
---
+++ assets/main.js
@@ -0,0 +1,22 @@
+theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+document.documentElement.setAttribute('data-bs-theme', theme)
+
+function errorAlert(errorMessage,inputId){
+    if ($('#alertfor' + inputId).length) {
+        if($('#alertfor' + inputId).html()==errorMessage){return}
+        $('#alertfor' + inputId).fadeOut('fast')
+        $('#alertfor' + inputId).html(errorMessage)
+    }
+    else {
+        $('#'+inputId).after('<div id="alertfor' + inputId + '" class="erroralert invalid-feedback">' + errorMessage + '</div')
+    }
+    $('#alertfor' + inputId).fadeIn('slow')
+    if (inputId=='') {return}
+    $('#'+inputId).addClass('is-invalid')
+}
+
+function formReset(formId){
+    $('#'+formId).trigger('reset')
+    $(':input','#'+formId).removeClass('is-invalid')
+    $('.erroralert','#'+formId).remove()
+}

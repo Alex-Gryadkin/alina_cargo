0c5ed76b2e4cadd9ff862347db9df326b93eb87e
alex-mazov <alexander.mazov@gmail.com>
2023-04-17 01:45:35 +0600

    packages: fix for packages.js

--- assets/packages.js
+++ assets/packages.js
@@ -3,7 +3,7 @@ var trackidAddModalEl = new bootstrap.Modal(document.getElementById('trackidAddM
 
 function DeleteTrackId(trackid) {
     $('#deltrackid').html(trackid)
-    $('#trackIdDelConfirmBtn'.).click(function(){
+    $('#trackIdDelConfirmBtn').click(function(){
         $.ajax({
             url:'del',
             type: 'get',
@@ -82,9 +82,9 @@ $(document).ready(function(){
                 else {
                     trackidAddModalEl.hide()
                     formReset('addpackageform')
-                    newTrackDiv = '<div class="card card' + response.status + ' mt-2 d-flex flex-row" id="track_' + response.packageid + '" style="display:none">'
+                    newTrackDiv = '<div class="card card' + response.status + ' mt-2 flex-row" id="track_' + response.packageid + '" style="display:none">'
                     newTrackDiv += '<div class="cardstart p-2 rounded-start"></div>'
-                    newTrackDiv += '<div class="card-body"><button type="button" class="btn-close float-end  btn-del" onclick="DeleteTrackId(\'' + response.packageid + '\')" aria-label="Удалить"></button>'
+                    newTrackDiv += '<div class="card-body"><button type="button" class="btn-close float-end btn-del" onclick="DeleteTrackId(\'' + response.packageid + '\')" aria-label="Удалить"></button>'
                     newTrackDiv += '<h3 class="card-title">' + (response.desc ? response.desc : response.packageid) + '</h3>'
                     newTrackDiv += '<h6 class="card-subtitle mb-2 text-body-secondary status-'+ response.status +'">' + response.statusname + ' ' + response.changedate + '</h6>'
                     newTrackDiv += '<p class="card-text">Трек-номер: ' + response.packageid + '</p></div></div>'

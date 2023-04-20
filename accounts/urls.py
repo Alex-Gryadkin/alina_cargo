8353325cb530e5a7d71403c01359f229aa6cbeff
Alex <thssfrlrnng@gmail.com>
2023-04-20 22:30:49 +0600

    password reset beginning

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
+    path('password_reset/', views.password_reset_view, name='password_reset')
 ]
--- packages/urls.py
+++ packages/urls.py
@@ -6,5 +6,6 @@ app_name = 'packages'
 urlpatterns = [
     path('', views.packages_list, name='list'),
     path('del',views.PackagesDelete.as_view(), name='del'),
-    path('add',views.PackagesAdd.as_view(), name='add')
+    path('add',views.PackagesAdd.as_view(), name='add'),
+
 ]

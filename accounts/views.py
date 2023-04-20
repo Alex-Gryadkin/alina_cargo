239832822d84938a18b1fa7013756867def2a5b2
alex-mazov <alexander.mazov@gmail.com>
2023-04-20 23:09:34 +0600

    fix for forms & views

--- accounts/views.py
+++ accounts/views.py
@@ -54,41 +54,37 @@ def register_view(request):
     elif request.GET.get('username'):
         username = request.GET.get('username')
         profile = User.objects.filter(username=username)
-        if not profile.exists():
-            try:
-                otp_session = OTPStorage.objects.get(phone=username)
-            except OTPStorage.DoesNotExist:
-                otp_session = None
-            if otp_session:
 
-                otp_session_created = otp_session.date_added
-                now = datetime.datetime.now(otp_session_created.tzinfo)
-                time_delta = datetime.datetime.timestamp(now) - datetime.datetime.timestamp(otp_session_created)
-
-                if time_delta > 60:
-                    out_otp = random.randint(100000, 999999)
-                    otp_session.date_added = now
-                    otp_session.otp = out_otp
-                    otp_session.save()
-                    sms = SMSC()
-                    sms.send_sms(f'7{username}', f'Проверочный код: {out_otp}')
-                    return render(request, 'OTP_input.html', {'username': username})
-
-                else:
-                    return render(request, 'OTP_input.html', {'username': username})
-
-            else:
-                out_otp = random.randint(100000, 999999)
-                otp_session = OTPStorage(phone=username, otp=out_otp)
-                otp_session.save()
-                sms = SMSC()
-                sms.send_sms(f'7{username}', f'Проверочный код: {out_otp}')
-                return render(request, 'OTP_input.html', {'username': username,
-                                                          'out_otp': out_otp})  # later - delete out_otp
-        else:
+        if profile.exists():
             user_exist_error = 'Пользователь с таким номером уже зарегистрирован'
             return render(request, 'register.html', {'form': form,
                                                      'error': user_exist_error})
+        try:
+            otp_session = OTPStorage.objects.get(phone=username)
+        except OTPStorage.DoesNotExist:
+            otp_session = None
+
+        out_otp = random.randint(100000, 999999)
+
+        if otp_session:
+            otp_session_created = otp_session.date_added
+            now = datetime.datetime.now(otp_session_created.tzinfo)
+            time_delta = datetime.datetime.timestamp(now) - datetime.datetime.timestamp(otp_session_created)
+
+            if time_delta < 60:
+                return render(request, 'OTP_input.html', {'username': username})
+
+            otp_session.date_added = now
+            otp_session.otp = out_otp
+
+        else:
+            otp_session = OTPStorage(phone=username, otp=out_otp)
+
+        otp_session.save()
+        sms = SMSC()
+        sms.send_sms(f'7{username}', f'Проверочный код: {out_otp}')
+        return render(request, 'OTP_input.html', {'username': username,
+                                                  'out_otp': out_otp})  # later - delete out_otp
 
     return render(request, 'register.html', {'form': form})
 
